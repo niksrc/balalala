29616fed086f9c8a2ba6e486a5496f18803e6cf6
Nikhil Srivastava <niksrc@gmail.com>
2015-06-28 18:14:13 +0530

    Add twitter login and mongolab persistence

--- app.js
+++ app.js
@@ -1,11 +1,65 @@
 var express = require('express');
+var session = require('express-session')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var passport = require('passport');
+var util = require('util');
+var TwitterStrategy = require('passport-twitter').Strategy;
+var Schema = mongoose.Schema;
 var app = express();
+
+require('dotenv').load();
+
+var UserSchema = new Schema({
+  provider: String,
+  uid: String,
+  name: String,
+  image: String,
+  created: {type: Date, default: Date.now}
+});
+
+mongoose.connect(process.env.MONGO_URL);
+mongoose.model('User', UserSchema);
+
+var User = mongoose.model('User');
+
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: process.env.TWITTER_CALLBACK_URL
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOne({uid: profile.id}, function(err, user) {
+      if(user) {
+        done(null, user);
+      } else {
+        var user = new User();
+        user.provider = "twitter";
+        user.uid = profile.id;
+        user.name = profile.displayName;
+        user.image = profile._json.profile_image_url;
+        user.save(function(err) {
+          if(err) { throw err; }
+          done(null, user);
+        });
+      }
+    })
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.uid);
+});
+
+passport.deserializeUser(function(uid, done) {
+  User.findOne({uid: uid}, function (err, user) {
+    done(err, user);
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +72,49 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'schrodingers cat',
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.get('/',function(req,res){
+  res.send("hello");
+})
+
+app.get('/home',function(req,res){
+  res.send(req.isAuthenticated() + " "+req.user);
+})
+
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // catch 404 and forward to error handler
