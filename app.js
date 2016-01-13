0489bd793e3fde17b1112d89588c3ae9bd3820ee
Nikhil Srivastava <niksrc@gmail.com>
2016-01-13 18:47:36 +0530

    Fix xo errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var session = require('express-session')
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,216 +7,97 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var util = require('util');
 var TwitterStrategy = require('passport-twitter').Strategy;
-var Twit = require('twit');
-var watson = require('watson-developer-cloud');
 require('dotenv').load();
 var User = require('./models/user');
-var Personality = require('./models/personality');
+var index = require('./routes/index');
+var search = require('./routes/search');
 var app = express();
 
 mongoose.connect(process.env.MONGO_URL);
 
-//Passport auth configuration
+// assport auth configuration
 passport.use(new TwitterStrategy({
-    consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: process.env.TWITTER_CALLBACK_URL
+	consumerKey: process.env.TWITTER_CONSUMER_KEY,
+	consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+	callbackURL: process.env.TWITTER_CALLBACK_URL
   },
-  function(token, tokenSecret, profile, done) {
-    User.findOne({uid: profile.id}, function(err, user) {
-      if(user) {
-        done(null, user);
-      } else {
-        var user = new User();
-        user.provider = "twitter";
-        user.uid = profile.id;
-        user.name = profile.displayName;
-        user.image = profile._json.profile_image_url;
-        user.save(function(err) {
-          if(err) { throw err; }
-          done(null, user);
-        });
-      }
-    })
+  function (token, tokenSecret, profile, done) {
+	User.findOne({uid: profile.id}, function (err, user) {
+		if (err) {
+			console.log(err);
+		}
+
+		if (user) {
+			done(null, user);
+		} else {
+			var newUser = new User();
+			newUser.provider = 'twitter';
+			newUser.uid = profile.id;
+			newUser.name = profile.displayName;
+			newUser.image = profile._json.profile_image_url;
+			newUser.save(function (err) {
+				if (err) {
+					throw err;
+				}
+				done(null, newUser);
+			});
+		}
+	});
   }
 ));
 
-passport.serializeUser(function(user, done) {
-  done(null, user.uid);
+passport.serializeUser(function (user, done) {
+	done(null, user.uid);
 });
 
-passport.deserializeUser(function(uid, done) {
-  User.findOne({uid: uid}, function (err, user) {
-    done(err, user);
-  });
-});
-
-//Personality insight conf
-var personality_insights = watson.personality_insights({
-  username: process.env.PI_USERNAME,
-  password: process.env.PI_PASSWORD,
-  version: 'v2'
+passport.deserializeUser(function (uid, done) {
+	User.findOne({uid: uid}, function (err, user) {
+		done(err, user);
+	});
 });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'schrodingers cat',
-  resave: false,
-  saveUninitialized: true,
+	secret: 'schrodingers cat',
+	resave: false,
+	saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// routes
+app.get('/', index);
+app.get('/search', search);
 
+// auth routes
+app.get('/auth/twitter', passport.authenticate('twitter'));
 
-
-//Routes
-app.get('/',function(req,res){
-  var authenticated = req.isAuthenticated();
-  res.render('index',{auth:authenticated,data:"badalalal"});
-})
-
-app.get('/home',function(req,res){
-  res.send(req.isAuthenticated() + " "+req.user);
-})
-
-
-app.get('/search',function(req,res){
-var p = new PersonalityService();
-var handle = req.query.handle.slice(1).toString();
-var score = 0 ;
-
-Personality.findOne({screenName: handle}, function(err, personality) {
-      if(err)
-        console.log(err);
-      if(personality) {
-          score = Math.round(parseInt(personality.data));
-          console.log(personality);
-          res.render('meter',{score:score,handle:handle})
-      } else {
-        p.get(undefined ,handle , function(data){
-          score = Math.round(data) || 0;
-          res.render('meter',{score:score,handle:handle})
-        }); 
-      }
-    })
+app.get('/auth/twitter/callback', passport.authenticate('twitter', {failureRedirect: '/login'
+}),
+function (req, res) {
+	res.redirect('/');
 });
 
-
-//Personality watson service
-function PersonalityService(){
-  var that = this;
-  var personality = new Personality();
-
-  var T = new Twit({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_SECRET
-  });  
-
-  this.getScore = function getScore(insights){
-    var activityLevel = insights.tree.children[0].children[0].children[2].children[0].percentage;
-    var cheerfulLevel = insights.tree.children[0].children[0].children[2].children[2].percentage;
-    var trustLevel = insights.tree.children[0].children[0].children[3].children[5].percentage;
-    var angerLevel = insights.tree.children[0].children[0].children[4].children[0].percentage;
-    var anxietyLevel = insights.tree.children[0].children[0].children[4].children[1].percentage;
-    var depressionLevel = insights.tree.children[0].children[0].children[4].children[2].percentage;
-    var vulnerabilityLevel = insights.tree.children[0].children[0].children[4].children[5].percentage;
-    var score;
-    score = 3 - activityLevel + cheerfulLevel + trustLevel + angerLevel + depressionLevel + vulnerabilityLevel
-    score = score / 6 ;
-    return score * 100 ;
-  }
-
-  this.get = function get(userid,screenName,cb) {
-    var userid = userid || "";
-    var screenName = screenName;
-    var params;
-    var score;
-
-    if(userid)
-      params = {user_id:user.id};
-    else
-      params = {screen_name:screenName};       
-    
-    T.get('statuses/user_timeline',params,function( err, data, response){
-      
-      if(err)
-        console.log(err);
-      else{
-        var text = '';
-        data.forEach(function(tweet){
-          text += tweet.text;
-        });
-
-        if(text.length < 100)
-          text += text;
-     
-
-        personality_insights.profile({
-          text: text },
-          function (err, response) {
-            if (err)
-              console.log('error:', err);
-            else{
-              score = that.getScore(response);
-              console.log(score);
-              cb(score)
-              personality.uid = userid;
-              personality.screenName = screenName;
-              personality.data = ""+score;
-              personality.save(function(err){
-                  if(err)
-                    console.log(err);
-                })
-            }
-        });
-      
-      }
-    
-    });    
-
-  }
-}
-
-
-
-
-
-
-
-//Auth routes
-app.get('/auth/twitter',
-  passport.authenticate('twitter'));
-
-app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+app.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -224,24 +105,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
-
 
 module.exports = app;
