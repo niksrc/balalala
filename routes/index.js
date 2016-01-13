e98edf2d267859e13e7366a7a343f46d32dcdb81
Nikhil Srivastava <niksrc@gmail.com>
2016-01-13 18:29:05 +0530

    Moved routes to separate file

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var router = require('express').Router();
+
+router.get('/', function (req, res) {
+	var authenticated = req.isAuthenticated();
+	res.render('index', {
+		auth: authenticated,
+		data: 'badalalal'
+	});
+});
+
+router.get('/home', function (req, res) {
+	res.send(req.isAuthenticated() + ' ' + req.user);
+});
+
+module.export = router;
