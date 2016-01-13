e80cefe56658405e72bb96a64261fcea0fcdc1be
Nikhil Srivastava <niksrc@gmail.com>
2016-01-13 18:27:54 +0530

    Refactored search routes

---
+++ routes/search.js
@@ -0,0 +1,37 @@
+var router = require('express').router;
+var PersonalityService = require('../lib/personalityService');
+var Personality = require('../models/personality');
+
+router.get('/search', function (req, res) {
+	var handle = req.query.handle.slice(1).toString();
+	var score = 0;
+
+	Personality.findOne({
+		screenName: handle
+	},
+	function (err, personality) {
+		if (err) {
+			console.log(err);
+		}
+
+		if (personality) {
+			score = Math.round(parseInt(personality.data, 10));
+
+			res.render('meter', {
+				score: score,
+				handle: handle
+			});
+		} else {
+			PersonalityService.fetch(undefined, handle, function (data) {
+				score = Math.round(data) || 0;
+
+				res.render('meter', {
+					score: score,
+					handle: handle
+				});
+			});
+		}
+	});
+});
+
+module.exports = router;
